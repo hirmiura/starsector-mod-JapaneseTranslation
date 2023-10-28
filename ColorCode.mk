3daa1e384a21c593e7774d514c42c689bdd502f3
Hiroshi Miura <hirmiura@gmail.com>
2023-10-28 23:10:52 +0900

    refactor(Makefile): カラーコードをMakefile本体から分離した

---
+++ ColorCode.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MIT
+# Copyright 2023 hirmiura (https://github.com/hirmiura)
+#==============================================================================
+# カラーコード
+#==============================================================================
+CC_Reset     := \033[0m
+CC_BrRed     := \033[91m
+CC_BrGreen   := \033[92m
+CC_BrYellow  := \033[93m
+CC_BrBlue    := \033[94m
+CC_BrMagenta := \033[95m
+CC_BrCyan    := \033[96m
+CC_BrWhite   := \033[97m
