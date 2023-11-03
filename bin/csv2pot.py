e27035dbefdfeae121b94780bffbd033023a32fc
Hiroshi Miura <hirmiura@gmail.com>
2023-11-03 22:28:04 +0900

    fix: 行番号の出力を修正した

--- bin/csv2pot.py
+++ bin/csv2pot.py
@@ -187,13 +187,11 @@ def generate_pot(config: ConfCsv2Pot) -> str:
     for file in config.input_files:
         with open(file, newline="", encoding="cp1252") as f:  # WARNING: Windows-1252 で読み込む
             reader = csv.reader(f)
-            line_num = 1
 
             # ヘッダがあれば読み込む
             headers = None
             if config.has_header:
                 headers = next(reader)
-                line_num += 1
 
             # 行を読み込む
             for row in reader:
@@ -202,7 +200,7 @@ def generate_pot(config: ConfCsv2Pot) -> str:
                     col = extract.column
                     if row_len > col and row[col].strip():
                         # リファレンス
-                        pottext.append(f"#: {file}:{line_num}")
+                        pottext.append(f"#: {file}:{reader.line_num}")
                         # フラグ
                         flags: Optional[str] = ",".join(extract.flags) if extract.flags else None
                         if flags is not None:
@@ -214,7 +212,6 @@ def generate_pot(config: ConfCsv2Pot) -> str:
                         # 文章
                         text = escape(row[col])
                         pottext.extend([f'msgid "{text}"', 'msgstr ""', ""])
-                line_num += 1
 
     # テキストを返す
     output_text = "\n".join(pottext)
